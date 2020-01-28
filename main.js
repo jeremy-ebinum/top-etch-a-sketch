62374bcd3abe7a7c9ae63ac2de36b4f99eff1f08
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-01-28 01:23:59 +0100

    Add options UI components

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const grid = document.querySelector(".js-grid");
-console.log(grid);
+const colorDropdown = document.querySelector(".js-dropdown");
+
+function clearGrid() {
+  console.log("CLEAR");
+}
+
+function setSize() {
+  console.log("SET SIZE");
+}
 
 function createGrid(size) {
   for (let i = 0; i < size; i++) {}
